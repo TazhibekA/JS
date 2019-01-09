51c5c2a08226cfff2e68282b6bb929c0a2f92fae
TazhibekA <40587394+TazhibekA@users.noreply.github.com>
2019-01-09 21:46:50 +0600

    Add files via upload

---
+++ JS/soft/project/func.js
@@ -0,0 +1,10 @@
+module.exports.test = function(a,b){
+  console.log(a+b);
+}
+
+module.exports.test2 = function(a,b){
+  console.log(a+b);
+}
+
+var t = 123;
+module.exports.t = t;
---
+++ JS/soft/project/index.js
@@ -0,0 +1,27 @@
+var http = require('http');
+
+
+
+
+//var func = require('./func');
+/*
+var fs = require('fs');
+
+fs.readFile('test.txt', 'utf8', function(err, data){
+  console.log(data);
+});
+console.log('=============================')
+
+var txt = "234234234234324";
+fs.appendFile('123.txt', `\n${txt}`, function() {
+
+});
+
+*/
+
+
+/*
+func.test(1,3);
+func.test2(1,3);
+console.log(func.t)
+*/
